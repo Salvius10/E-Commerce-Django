c8ea9cf4e0442577ce8235bc31d204c9f298e729
Salvius10 <melvinsalvius.i@gmail.com>
2025-06-18 00:52:20 +0530

    Admin Panel Commit

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import RegisterUserView,ProfileUserView
+from .views import RegisterUserView,ProfileUserView,AdminCartView,AdminOrderView,AdminUserView,AdminProductView,AdminUserListView
 urlpatterns=[
     path("register/",RegisterUserView.as_view(),name="register"),
     path("profile/", ProfileUserView.as_view(), name="profile"),
+    path('admin/user/<int:pk>/', AdminUserView.as_view()),
+    path('admin/product/<int:pk>/', AdminProductView.as_view()),
+    path('admin/order/<int:pk>/', AdminOrderView.as_view()),
+    path('admin/cart/<int:pk>/', AdminCartView.as_view()),
+    path('admin/users/', AdminUserListView.as_view()), 
+
+
 
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated,AllowAny
+from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser
+from products.models import ProductModel
+from products.serializers import ProductView
+from orders.serializers import OrderSerializer
+from orders.models import Order
+from carts.serializers import CartSerializer
+from carts.models import Cart
+
 # Create your views here.
 
 class RegisterUserView(generics.CreateAPIView):
@@ -16,3 +23,28 @@ class ProfileUserView(generics.RetrieveAPIView):
 
     def get_object(self):
         return self.request.user
+
+class AdminUserView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+    permission_classes=[IsAdminUser]
+
+class AdminUserListView(generics.ListAPIView):
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+    permission_classes=[IsAdminUser]
+
+class AdminProductView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=ProductModel.objects.all()
+    serializer_class=ProductView
+    permission_classes=[IsAdminUser]
+
+class AdminOrderView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Order.objects.all()
+    serializer_class=OrderSerializer
+    permission_classes=[IsAdminUser]
+
+class AdminCartView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Cart.objects.all()
+    serializer_class=CartSerializer
+    permission_classes=[IsAdminUser]
